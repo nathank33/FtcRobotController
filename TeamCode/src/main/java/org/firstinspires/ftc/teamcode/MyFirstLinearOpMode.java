bdafd1c0577e3d72b502239a414ff82a0d817395
Nathan Braun <nathanbraunguitar@gmail.com>
2023-09-18 17:40:22 -0700

    Add an DcMotor to MyFirstLinearOpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstLinearOpMode.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class MyFirstLinearOpMode extends LinearOpMode {
+    private DcMotor motorTest;
+
     @Override
     public void runOpMode() {
+        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
     }
 }
