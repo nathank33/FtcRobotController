234fd471de7cafeb952b79fa40865f0071c5377f
Nathan Braun <nathanbraunguitar@gmail.com>
2023-09-18 17:39:02 -0700

    Add an imu to MyFirstLinearOpMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstLinearOpMode.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gyroscope;
 
 public class MyFirstLinearOpMode extends LinearOpMode {
+    private Gyroscope imu;
+
     @Override
     public void runOpMode() {
+        imu = hardwareMap.get(Gyroscope.class, "imu");
     }
 }
